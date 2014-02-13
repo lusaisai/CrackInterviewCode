6c44973cd5d0f9859906035e6dad3fff20e39bd9
lusaisai <lusaisai@163.com>
2014-02-13 21:33:26 +0800

    format the javadoc a little

--- src/main/java/CrackInterviewCode/ArraysAndStrings.java
+++ src/main/java/CrackInterviewCode/ArraysAndStrings.java
@@ -16,15 +16,15 @@ public class ArraysAndStrings {
      *
      * <p>
      *     One way is to iterate through the string, for each character, compare it with all previous characters.
-     *     It requires 1+2+3+ ... + n times' comparison, that results in O(n2) running time.
+     *     It requires 1+2+3+ ... + n times' comparison, that results in O(n2) running time.<br/>
      *     Another way is to sort the string, the duplicate characters will always be adjacent.
-     *     It requires O(nlogn) for sort, plus O(n) times' comparison, still O(nlogn).
+     *     It requires O(nlogn) for sort, plus O(n) times' comparison, still O(nlogn).<br/>
      *     Both works but with a bad performance. A better way is to use a hash, if we're allowed.
      *     Then we can iterate through the string, check the character in the hash to see if it already exists, store it if not.
-     *     Hash provides O(1) time storing and searching, so this provides o(n) running time.
+     *     Hash provides O(1) time storing and searching, so this provides o(n) running time.<br/>
      *     But the question says we cannot use additional data structures. How do we do it? Say if the string is
      *     not unicode, just ascii characters, there are only 256 possible values, we can use an array of size 256,
-     *     making it work like a hash, the character stores in the position of its ascii value. This method is the implementation in this case.
+     *     making it work like a hash, the character stores in the position of its ascii value. This method is the implementation in this case.<br/>
      *     An even better performed way is to use bit, every bit can act as a flag, from byte to bit, it saves the space by 8 times.
      * </p>
      * @param s The input string to be detected
