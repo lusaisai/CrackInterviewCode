174e2981d81ee7e7d29363b3c64a881f68cabee6
lusaisai <lusaisai@163.com>
2014-02-13 22:23:26 +0800

    save space using bit

--- src/main/java/CrackInterviewCode/ArraysAndStrings.java
+++ src/main/java/CrackInterviewCode/ArraysAndStrings.java
@@ -25,7 +25,7 @@ public class ArraysAndStrings {
      *     But the question says we cannot use additional data structures. How do we do it? Say if the string is
      *     not unicode, just ascii characters, there are only 256 possible values, we can use an array of size 256,
      *     making it work like a hash, the character stores in the position of its ascii value. This method is the implementation in this case.<br/>
-     *     An even better performed way is to use bit, every bit can act as a flag, from byte to bit, it saves the space by 8 times.
+     *     An even better performed way is to use bit, every bit can act as a flag, from byte to bit, it saves the space by 8 times. see method hasDuplicate2.
      * </p>
      * @param s The input string to be detected
      * @return if it has duplicate characters or not
@@ -41,6 +41,29 @@ public class ArraysAndStrings {
                 return true;
             } else {
                 bucket[(int)c] = true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * An alternative way for detecting duplicate characters using bit to save space.
+     * @param s The input string to be detected
+     * @return if it has duplicate characters or not
+     */
+    public static boolean hasDuplicate2(String s) {
+        int[] bucket = new int[8]; // java int data type is 32-bit, 32 * 8 is 256, I will use the rightmost bit to store ascii 0, leftmost for ascii 255
+        Arrays.fill(bucket, 0);
+
+        char[] input = s.toCharArray();
+        for( char c : input ) {
+            int index = 7 - (int) c / 32; // the position in the bucket array
+            int shift = (int) c % 32; // the number of positions in the 32 bit(in the direction from right to left)
+            if ( ( bucket[index] & (1 << shift) ) > 0 ) {
+                return  true;
+            } else {
+                bucket[index] |= (1 << shift);
             }
         }
 
--- src/test/java/CrackInterviewCode/ArraysAndStringsTest.java
+++ src/test/java/CrackInterviewCode/ArraysAndStringsTest.java
@@ -2,9 +2,6 @@ package CrackInterviewCode;
 
 import junit.framework.TestCase;
 
-/**
- * Created by lusaisai on 14-2-13.
- */
 public class ArraysAndStringsTest extends TestCase {
 
     public ArraysAndStringsTest(String name) {
@@ -14,6 +11,8 @@ public class ArraysAndStringsTest extends TestCase {
     public void testHasDuplicate() throws Exception {
         assertEquals( true, ArraysAndStrings.hasDuplicate("1234567890-=][poiuyhgfrdewqav1") );
         assertEquals( false, ArraysAndStrings.hasDuplicate("1234567890-=][poiuyhgfrdewqav") );
-        assertEquals( false, ArraysAndStrings.hasDuplicate("/*-789456123+") );
+
+        assertEquals( true, ArraysAndStrings.hasDuplicate2("1234567890-=][poiuyhgfrdewqav1") );
+        assertEquals( false, ArraysAndStrings.hasDuplicate2("1234567890-=][poiuyhgfrdewqav") );
     }
 }
